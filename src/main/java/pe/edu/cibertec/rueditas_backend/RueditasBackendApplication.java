2da51a12bf1e597fe250c937a55505ae2e56482e
mileidylaura <mileidylaura150@gmail.com>
2024-09-26 11:27:25 -0500

    Creando el nuevo Backend Rueditas

---
+++ src/main/java/pe/edu/cibertec/rueditas_backend/RueditasBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasBackendApplication.class, args);
+	}
+
+}
